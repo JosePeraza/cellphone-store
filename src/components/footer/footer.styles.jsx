f88aa0fe44eea3a58ee6395b70077a46f4ab3291
José Peraza <joseperazarivero@gmail.com>
2022-07-14 09:19:45 -0500

    fixing routers

--- src/components/footer/footer.styles.jsx
+++ src/components/footer/footer.styles.jsx
@@ -4,6 +4,7 @@ export const FooterContainer = styled.div`
   width: 100%;
   display: flex;
   min-height: 100px;
+  margin-top: 15px;
   padding: 15px 0;
   font-size: 20px;
   align-items: center;
